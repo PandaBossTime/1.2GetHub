01fab464ef46b73a265aa661d50d124c3ccbb2a3
black-fox-2005@bk.ru <black-fox-2005@bk.ru>
2024-11-16 14:52:20 +1000

    add calc ui те файлы проекта.

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,34 +21,6 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-            private string imgsource1 = @"\\sysprofiles.adm.vvsu.ru\STUDENTRPROFILES$\PandaBossTime\My Pictures\1589114980_preview_subnautica-okean-sushchestvo-batiskaf-glaza-zmeia.jpg";
-            private string imgsource2 = @"\\sysprofiles.adm.vvsu.ru\STUDENTRPROFILES$\PandaBossTime\My Pictures\31.png";
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-    private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Window2 window = new Window2();
-            window.Show();
-            this.Close();
-
-        }
-
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-           
-        }
-
-        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
-        {
-            DisplayImage.Source = new BitmapImage(new Uri(imgsource1));
-        }
-
-        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
-        {
-            DisplayImage.Source = new BitmapImage(new Uri(imgsource2));
-        }
     }
 }
+
